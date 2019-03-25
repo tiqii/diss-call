0d31fb36579337491d4584ff27f2d3f8464babe2
Paul <cn.paullee@gmail.com>
2019-03-25 22:44:34 +0800

    日常同步

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,4 +1,4 @@
-from . import views
 from flask import Blueprint
 
 main = Blueprint('main', __name__)
+from . import views
--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,18 @@
-from flask import render_template
+from flask import render_template, request, jsonify
 from . import main
 
 
-@main.route('/')
+@main.route('/', methods=["GET"])
 def index():
-    """ index路由 """
-    return render_template('index.html')
+    """ 
+    获取需要diss的电话号码，做diss请求后返回diss状态
+    """
+    # 获取需要diss的号码
+    phone_num = request.args['phone_num']
+
+    # 这里做diss操作
+    # TODO: 接入diss操作
+
+    # 返回
+    res = {"status": 201, "msg": "success", "data": {"phone": phone_num}}
+    return jsonify(res)
